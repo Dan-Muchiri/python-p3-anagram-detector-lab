2b3c6216de2dd71f6bb1936bdccd00617e6e6534
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2024-03-05 12:59:45 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        anagrams = []
+        for candidate in candidates:
+            candidate_lower = candidate.lower()
+            if self.word != candidate_lower and sorted(self.word) == sorted(candidate_lower):
+                anagrams.append(candidate)
+        return anagrams
